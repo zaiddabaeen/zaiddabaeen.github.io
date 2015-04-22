8d13e76cee44f317cbd1271f7e13afb104db2604
zaiddabaeen <zaid@redtroops.com>
2015-04-22 11:31:04 +0300

    Removed e.preventDefault() for swiping

--- js/swipe.js
+++ js/swipe.js
@@ -21,11 +21,11 @@ function swipedetect(el, callback){
         startX = touchobj.pageX
         startY = touchobj.pageY
         startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
+        //e.preventDefault()
     }, false)
   
     touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
+       // e.preventDefault() // prevent scrolling when inside DIV
     }, false)
   
     touchsurface.addEventListener('touchend', function(e){
@@ -42,6 +42,6 @@ function swipedetect(el, callback){
             }
         }
         handleswipe(swipedir)
-        e.preventDefault()
+        //e.preventDefault()
     }, false)
 }
